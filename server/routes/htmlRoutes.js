e247cc8bb53eed9f342aa7c0605710f4828e5b09
unknown <mescamilla1114@gmail.com>
2023-03-06 14:45:07 -0600

    changed html routes

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>{
-  const index = path.join(__dirname, 'build', '../client/dist/index.html');
+  const index = path.join(__dirname, 'build', 'index.html');
   res.sendFile(index);
   }
     //res.sendFile(path.join(__dirname, '../client/dist/index.html'))
